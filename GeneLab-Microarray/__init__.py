79f7d8cf97314fca6f024fc10318a0025dda1cb2
joru1876 <jonathan.rubin@colorado.edu>
2018-06-13 16:23:40 -0700

    debugged pip install. Now you can do a pip install . in the root directory of this repo and run GeneLab-Microarray --help

--- GeneLab-Microarray/__init__.py
+++ GeneLab-Microarray/__init__.py
@@ -1,1 +1,5 @@
+import __main__
 
+
+def run():
+    __main__.run()
--- GeneLab-Microarray/__main__.py
+++ GeneLab-Microarray/__main__.py
@@ -1,3 +1,16 @@
-import main
+__author__ = 'Jonathan Rubin'
 
-main.run()
+import argparse
+import sys
+
+def run():
+    parser = argparse.ArgumentParser(prog='GeneLab-Microarray',usage='%(prog)s [-c ChipType]',description='Standardized processing pipeline for microarray data on GeneLab.')
+    parser.add_argument('-c','--chip',help='Specify the type of microarray chip. Options: Affymetrix, Agilent, Nimblegen, Illumina, Custom',
+        choices=['Affymetrix', 'Agilent', 'Nimblegen', 'Illumina', 'Custom'],metavar='',required=True)
+    if len(sys.argv)==1:
+        # display help message when no args are passed. 
+        parser.print_help()
+        sys.exit(1)
+
+    chip = parser.parse_args().chip
+    print chip
