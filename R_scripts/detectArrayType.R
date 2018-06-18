a4b329a5d3d4f68e632e0c8283c3abea61e85365
Daniel Mattox <xfdmattox@gmail.com>
2018-06-18 11:16:26 -0700

    risa stuff

--- R_scripts/detectArrayType.R
+++ R_scripts/detectArrayType.R
@@ -7,6 +7,7 @@ require(Risa)
 
 rsaIN = readISAtab("~/Documents/genelab/rot1/GLDS-4/metadata/GLDS-4_metadata_GSE18388-ISA/")
 
-factors = rsaIN@study.files[[1]]
+sampFactors = rsaIN@study.files[[1]]
+assayFactors = rsaIN@assay.files[[1]]
 
 # May not be feasible tro detect array type but above command will extract factors
