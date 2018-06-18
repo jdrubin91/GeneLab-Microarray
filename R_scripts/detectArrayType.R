105046c5466f0a53618e030dbdd9a0144f0b25d0
Daniel Mattox <xfdmattox@gmail.com>
2018-06-18 11:13:08 -0700

    risa stuff

---
+++ R_scripts/detectArrayType.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+# source("http://bioconductor.org/biocLite.R")
+# biocLite("Risa")
+
+require(Risa)
+
+rsaIN = readISAtab("~/Documents/genelab/rot1/GLDS-4/metadata/GLDS-4_metadata_GSE18388-ISA/")
+
+factors = rsaIN@study.files[[1]]
+
+# May not be feasible tro detect array type but above command will extract factors
