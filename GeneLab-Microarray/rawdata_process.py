515fc152fc74388a8e2badda1a55083b91ac21f4
joru1876 <jonathan.rubin@colorado.edu>
2018-06-21 14:08:52 -0700

    trouble with config variables, trying a different approach

--- GeneLab-Microarray/rawdata_process.py
+++ GeneLab-Microarray/rawdata_process.py
@@ -86,7 +86,6 @@ def qc_and_normalize(rawdata_out,GLDS):
         sys.exit(1)
 
 def limma_differential(rawdata_out,metadata_out,GLDS):
-    import config
     condition1,condition2,pval_cut = config.visualize.split(',')
     limma_script = os.path.join(config.R_dir,'limmaDiffExp.R')
     if GLDS + "_microarray_normalized_annotated.txt" in os.listdir(rawdata_out):
@@ -97,8 +96,8 @@ def limma_differential(rawdata_out,metadata_out,GLDS):
         print "Error: No expression count file detected, exiting..."
         sys.exit(1)
     r_option = " -i " + metadata_out
-    group1_option = " --group1=" + config.condition1
-    group2_option = " --group2=" + config.condition2
+    group1_option = " --group1=" + condition1
+    group2_option = " --group2=" + condition2
     o_option = " -o " + GLDS + "_microarray_DGE.txt"
     limma_differential_command = "Rscript --vanilla " + limma_script + d_option + r_option + group1_option + group2_option + o_option
     print limma_differential_command
