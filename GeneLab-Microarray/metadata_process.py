8580f5c39663c7b00d3e07f1e90f518ce5976497
joru1876 <jonathan.rubin@colorado.edu>
2018-06-26 18:22:34 -0700

    wasn't unzipping... now doing it

--- GeneLab-Microarray/metadata_process.py
+++ GeneLab-Microarray/metadata_process.py
@@ -32,7 +32,7 @@ def clean(metadata_directory):
 
             #Check md5sum of original zip file
             md5sum_command = ["md5sum",metadata_zip]
-            original_md5sum = subprocess.check_output(md5sum_command).split(' ')[0].decode("utf-8")
+            original_md5sum = subprocess.check_output(md5sum_command).split(' ')[0]
             config.md5sum["original"].append((zip_filename,original_md5sum))
 
             #Copy the last modified metadata
@@ -49,12 +49,12 @@ def clean(metadata_directory):
 
             #Verify md5sum for 'new' file
             md5sum_command = ["md5sum",os.path.join(metadata_out,zip_filename)]
-            new_md5sum = subprocess.check_output(md5sum_command).split(' ')[0].decode("utf-8")
+            new_md5sum = subprocess.check_output(md5sum_command).split(' ')[0]
             config.md5sum["new"].append((zip_filename,new_md5sum))
             print config.md5sum
 
             #Execute unzipping and zip removal commands
-            # subprocess.call(unzip_command)
+            subprocess.call(unzip_command)
             # subprocess.call(remove_zip_command)
 
             i += 1
