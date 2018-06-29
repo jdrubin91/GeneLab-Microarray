77ff45f057ced299b07faf72ad2ef1a96a435d7e
Daniel Mattox <xfdmattox@gmail.com>
2018-06-29 14:03:59 -0700

    progress statements and cleaned saving

--- GeneLab-Microarray/R_scripts/extractRawTwoColor.R
+++ GeneLab-Microarray/R_scripts/extractRawTwoColor.R
@@ -24,20 +24,32 @@ if (is.null(opt$output)){ # Check and set output file handle
   stop("No path to out file provided. Please look over the available options\n", call. = F)
 }else outFH = opt$output
 
-
+cat("Reading in file...")
 test = read.delim(inFH, stringsAsFactors = F, header = F) # Read in processed file
 
+cat("Extracting raw values...\n")
 G = test[,grep("gMedianSignal",test)] # Idenitfy median foreground intensity columns
+cat("-")
 R = test[,grep("rMedianSignal",test)]
+cat("-")
 Gb = test[,grep("gBGMedianSignal",test)] # Idenitfy median background intensities columns
+cat("-")
 Rb = test[,grep("rBGMedianSignal",test)]
+cat("-")
 startInd = grep("gMedianSignal",G) + 1 # Indentify starting row index of raw values
+cat("-")
 G = G[startInd:length(G)] # Extract raw values
+cat("-")
 R = R[startInd:length(R)]
+cat("-")
 Gb = Gb[startInd:length(Gb)]
+cat("-")
 Rb = Rb[startInd:length(Rb)]
+cat("-")
 raw = cbind(R,G,Rb,Gb) # Bind raw value vectors into dataframe
+cat("-")
 row.names(raw) = test[startInd:nrow(test),grep("FeatureNum", test)] # Add unique feature numbers for mapping to genes
+cat("-\n")
 
-write.table(raw,file = outFH)
-
+write.table(raw,file = outFH,sep="\t",quote = F)
+cat("Success! Raw values saved to:",outFH)
