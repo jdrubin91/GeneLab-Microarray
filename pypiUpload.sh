2da61256546b1acc80bc18066a71f30e5a64e0a0
joru1876 <jonathan.rubin@colorado.edu>
2018-07-09 13:45:29 -0700

    Added bash script for easy pypi uploading

---
+++ pypiUpload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Run within a directory containing setup.py
+
+python setup.py sdist bdist_wheel
+python /Users/jonathanrubin/Library/Python/2.7/lib/python/site-packages/twine upload dist/*
