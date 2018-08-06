69c917004243642891c9e0c63f8585885c1e2afb
joru1876 <jonathan.rubin@colorado.edu>
2018-08-06 15:42:04 -0700

    md5sum should work with parentheses and spaces in filenames

--- GeneLab-Microarray/rawdata_process.py
+++ GeneLab-Microarray/rawdata_process.py
@@ -117,7 +117,7 @@ def rename(GLDS_path):
             #If the filename is an annotation type, don't include 'raw' in filename
             if '.adf.' in filename or 'GPL' in filename:
                 new_filename = filename.replace('_','-').replace('(','-').replace(')','-').replace(' ','-').replace(GLDS,'').replace('microarray','').replace('--','-').replace('.adf','-adf').strip('-').split('.')[0]
-                move_command = ["mv", "'"+os.path.join(rawdata_out,filename).replace(' ','\\ ')+"'", os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_annotation.'+extension)]
+                move_command = ["mv", "'"+os.path.join(rawdata_out,filename)+"'", os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_annotation.'+extension)]
                 new_md5sum_file = os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_annotation.'+extension)
             else:
                 for key in assay_dict:
@@ -125,7 +125,7 @@ def rename(GLDS_path):
                     if key in filename:
                         sample_in_first_column = True
                         sample_name = key.replace(' ','-').replace('_','-').replace('(','-').replace(')','-').replace(GLDS,'').replace('microarray','').replace('--','-').strip('-')
-                        move_command = ["mv", "'"+os.path.join(rawdata_out,filename).replace(' ','\\ ')+"'", os.path.join(final_rawdata_out,GLDS+'_'+sample_name+'_microarray_raw.'+extension)]
+                        move_command = ["mv", "'"+os.path.join(rawdata_out,filename)+"'", os.path.join(final_rawdata_out,GLDS+'_'+sample_name+'_microarray_raw.'+extension)]
                         new_md5sum_file = os.path.join(final_rawdata_out,GLDS+'_'+sample_name+'_microarray_raw.'+extension)
 
                 #If the first column does not correspond to any filenames, look in other columns. Still rename as first column without special characters
@@ -135,13 +135,13 @@ def rename(GLDS_path):
                             if item == filename and item != '':
                                 sample_in_other_column = True
                                 new_filename = key.replace('_','-').replace('(','-').replace(')','-').replace(' ','-').replace(GLDS,'').replace('microarray','').replace('--','-').strip('-')
-                                move_command = ["mv","'"+os.path.join(rawdata_out,filename).replace(' ','\\ ')+"'", os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_raw.'+extension)]
+                                move_command = ["mv","'"+os.path.join(rawdata_out,filename)+"'", os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_raw.'+extension)]
                                 new_md5sum_file = os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_raw.'+extension)
 
                 #If the filename isn't in the metadata, just remove special characters and append appropriate information
                 if not sample_in_first_column and not sample_in_other_column:
                     new_filename = filename.replace('_','-').replace('(','-').replace(')','-').replace(' ','-').replace(GLDS,'').replace('microarray','').replace('--','-').strip('-').split('.')[0]
-                    move_command = ["mv","'"+os.path.join(rawdata_out,filename).replace(' ','\\ ')+"'", os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_other.'+extension)]
+                    move_command = ["mv","'"+os.path.join(rawdata_out,filename)+"'", os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_other.'+extension)]
                     new_md5sum_file = os.path.join(final_rawdata_out,GLDS+'_'+new_filename+'_microarray_other.'+extension)
 
             #Execute the command if the file was in metadata - catch whether the file already exists and don't output an error
