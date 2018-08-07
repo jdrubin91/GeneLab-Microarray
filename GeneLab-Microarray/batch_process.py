ca9d70ba4890ca7f454f0f672b78200390474712
joru1876 <jonathan.rubin@colorado.edu>
2018-08-07 10:13:12 -0700

    debugging

--- GeneLab-Microarray/batch_process.py
+++ GeneLab-Microarray/batch_process.py
@@ -40,6 +40,7 @@ def run(batch_file):
 
                 #Copy rawdata into output
                 print os.listdir(os.path.join(parent_dir,GLDS))
+                print ['microarray' in x for x in os.listdir(os.path.join(parent_dir,GLDS))]
                 rawdata_in = os.path.join(parent_dir,GLDS,'microarray')
                 if os.path.isdir(rawdata_in):
                     rawdata_process.copy(rawdata_in)
